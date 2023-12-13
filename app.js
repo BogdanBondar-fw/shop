6460888ca5be81eabae4cc8537737e1b4f1fdb13
Bogdan <107194578+BogdanBondar-fw@users.noreply.github.com>
2023-12-13 13:04:37 +0200

    1

---
+++ app.js
@@ -0,0 +1,90 @@
+let productsCountEL = document.getElementById("products-count");
+
+let addToCartBtns = document.querySelectorAll(".btn-add-to-car");
+
+addToCartBtns.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    productsCountEL.textContent = +productsCountEL.textContent + 1;
+  })
+);
+
+let addLikeToDevice = document.querySelectorAll(".like");
+
+addLikeToDevice.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    btn.classList.toggle("liked");
+  })
+);
+
+let btnMoreDetails = document.querySelectorAll(".btn-more-details");
+
+let modal = document.querySelector(".modal");
+
+btnMoreDetails.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    modal.classList.remove("hide");
+    modal.classList.add("show");
+  })
+);
+
+let modalClose = document.querySelector(".btn-close");
+
+modalClose.addEventListener("click", function () {
+  modal.classList.remove("show");
+  modal.classList.add("hide");
+});
+
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    modal.classList.remove("show");
+    modal.classList.add("hide");
+  }
+});
+
+$(".slider").slick({
+  dots: true,
+});
+
+let decrementBtns = document.querySelectorAll(".decrement-button");
+let incrementBtns = document.querySelectorAll(".increment-button");
+let inputFields = document.querySelectorAll(".product-quantity");
+
+function Counter(incrementBtn, decrementBtn, inputField) {
+  this.domRefs = {
+    incrementBtn,
+    decrementBtn,
+    inputField,
+  };
+
+  this.increment = function () {
+    this.domRefs.inputField.value = +this.domRefs.inputField.value + 1;
+    this.toggleButtonState();
+  };
+
+  this.decrement = function () {
+    this.domRefs.inputField.value = +this.domRefs.inputField.value - 1;
+    this.toggleButtonState();
+  };
+
+  this.toggleButtonState = function () {
+    let count = this.domRefs.inputField.value;
+    this.domRefs.decrementBtn.disabled = count <= 1;
+    this.domRefs.incrementBtn.disabled = count >= 10;
+  };
+
+  this.domRefs.incrementBtn.addEventListener(
+    "click",
+    this.increment.bind(this)
+  );
+
+  this.domRefs.decrementBtn.addEventListener(
+    "click",
+    this.decrement.bind(this)
+  );
+
+  this.toggleButtonState();
+}
+
+for (let i = 0; i < decrementBtns.length; i++) {
+  let counter = new Counter(incrementBtns[i], decrementBtns[i], inputFields[i]);
+}
